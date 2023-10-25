cfee9e8df4451dccca6afebece099c5bfc24163b
UserSampath <nalakasampathsmp@gmail.com>
2023-10-26 01:45:20 +0530

    add nav bar

--- Frontend/src/pages/home/Home.jsx
+++ Frontend/src/pages/home/Home.jsx
@@ -6,7 +6,27 @@ const Home = () => {
   return (
     <>
       <Nav category="customer" />
-
+      <div className="boxMiddle">
+        <h1
+          className="mainHeading"
+          style={{ marginTop: "20px", marginBottom: "20px" }}>
+          Inventory
+        </h1>
+      </div>
+      <div className="boxMiddle">
+        <div
+          className="itemContainer"
+          style={{
+            width: "1000px",
+            height: "90px",
+            background: "rgba(171, 205, 239, 0.1)",
+            border: "1px solid black",
+            borderRadius: "20px",
+            padding: "8px",
+          }}>df
+          
+          </div>
+      </div>
     </>
   );
 };
