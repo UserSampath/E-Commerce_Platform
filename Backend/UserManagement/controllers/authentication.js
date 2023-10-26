d8d5af8192e0090cff635db2ba3715de1b729ef6
Chanaka <chanaka7518@gmail.com>
2023-10-26 19:41:21 +0530

    Added user login and registration

---
+++ Backend/UserManagement/controllers/authentication.js
@@ -0,0 +1,56 @@
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const register = async (req, res, next) => {
+  const { password, email } = req.body;
+
+  let user;
+  console.log("hiiiiiiiiiiii");
+  try {
+    user = await User.findOne({ email: email });
+
+    if (user) return next(createError(409, "User already exists"));
+
+    const hashedPassword = bcrypt.hashSync(password, 12);
+    const newUser = new User({
+      ...req.body, // in the body passs the user roll
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+    res.status(201).send("New user has been created!");
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+const login = async (req, res, next) => {
+  const { email } = req.body;
+
+  try {
+    // try to find the user in the clients collection
+    let user = await User.findOne({ email: email });
+
+    if (!user) return next(createError(404, "User Not found"));
+
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordValid)
+      return next(
+        createError(400, "Wrong password or username.Please try again!")
+      );
+
+    // to prevent send password to the user.send details without password
+    const { password, ...info } = user._doc;
+    res.status(200).send(info);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { register, login };
---
+++ Backend/UserManagement/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
+
+const server = express();
+
+server.use(bodyParser.json({ limit: "32mb", extended: true }));
+server.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
+server.use(cors());
+
+server.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+const pwd = "usermanagement";
+const MONGO_URI = `mongodb+srv://chanaka7518:${pwd}@cluster0.ntppbya.mongodb.net/?retryWrites=true&w=majority`;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    server.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Connection to mongoDB faild", error.message);
+  }
+};
+
+connectDB();
+
+mongoose.connection.on("open", () => {
+  console.log("Connection to database has been established");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
