8ed9d4845e20d1ddeb339446967ff4a3173900a7
IsHaNinfo <ishaninfo56@gmail.com>
2023-11-03 19:45:55 +0530

    rearrange myorder

--- Frontend/src/pages/myorderPage/MyOrder.jsx
+++ Frontend/src/pages/myorderPage/MyOrder.jsx
@@ -2,32 +2,48 @@ import React from "react";
 import "./myOrder.css";
 import { Nav } from "../../components/Nav/Nav";
 import Button from "../../components/Button/Button";
-import InventoryItem from "../../components/InventoryItem/InventoryItem";
+import ProductItem from "../../components/ProductItem/ProductItem";
 
 const items = [
   {
     name: "Asus laptop",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur numquam corporis ducimus quibusdam architecto molestiae autem earum doloribus vero, asperiores, laborum dolorum aliquid deserunt recusandae totam. Est, eius molestiae.",
-    quantity: 24,
+      "8GB RAM, 1TB ROMr",
+      quantity: 24,
+    address:"Colombo",
+    price:"$60.70",
+    status:"pickup",
+    deliverExpectedby:"24th December 2023"
   },
   {
     name: "Asus laptop",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur numquam corporis ducimus quibusdam architecto molestiae autem earum doloribus vero, asperiores, laborum dolorum aliquid deserunt recusandae totam. Est, eius molestiae.",
+      "8GB RAM, 1TB ROM,  .",
     quantity: 24,
+    address:"Colombo",
+    price:"$60.70",
+    status:"pickup",
+    deliverExpectedby:"24th December 2023"
   },
   {
     name: "Asus laptop",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur numquam corporis ducimus quibusdam architecto molestiae autem earum doloribus vero, asperiores, laborum dolorum aliquid deserunt recusandae totam. Est, eius molestiae.",
+      "8GB RAM, 1TB ROM. .",
     quantity: 24,
+    address:"Colombo",
+    price:"$60.70",
+    status:"pickup",
+    deliverExpectedby:"24th December 2023"
   },
   {
     name: "Asus laptop",
     description:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad tenetur numquam corporis ducimus quibusdam architecto molestiae autem earum doloribus vero, asperiores, laborum dolorum aliquid deserunt recusandae totam. Est, eius molestiae.",
+      "8GB RAM, 1TB ROM, .",
     quantity: 24,
+    address:"Colombo",
+    price:"$60.70",
+    status:"pickup",
+    deliverExpectedby:"24th December 2023"
   },
 
 ];
@@ -36,23 +52,26 @@ const MyOrder = () => {
     return (
       <>
         <Nav category="customer" />
-        <div className="boxMiddle">
-            <div className="mainHeading">
+        <br/>
+        <br/>
+        <div className="mainHeading">
           <h1>
             My Orders
           </h1>
           </div>
+        <div className="boxMiddle">
+          
           <div className="Orderrow" >
             <h1 className="item">Item</h1>
-            <h1>Qty</h1>
-            <h1>Address</h1>
+            <h1 className="qty">Qty</h1>
+           
             <h1>Status</h1>
             <h1>Deliver Expected by</h1>
           </div>
         </div>
         {items.map((item,index) => {
           return (
-            <InventoryItem key={index} name={item.name} description={item.description} quantity={item.quantity} />
+            <ProductItem key={index} name={item.name} description={item.description} price={item.price}quantity={item.quantity}  status={item.status} deliverExpectedby={item.deliverExpectedby}/>
           )
         })}
       </>
