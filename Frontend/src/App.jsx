874ae903312c1eeef0e2b7e9e7bcf489d87b0020
PrabathAnuradhaDharmasiri <dharmasiripa.20@uom.lk>
2023-11-03 20:43:15 +0530

    Cart

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from "./pages/signIn/SignIn";
 import SignUp from "./pages/signUp/SignUp";
 import Home from "./pages/home/Home";
 import About from "./pages/About/About"; 
+import Cart from "./pages/Cart/Cart"
 
 function App() {
   return (
@@ -13,8 +14,7 @@ function App() {
         <Route path="/Home" element={<Home />}></Route>
         <Route path="/register" element={<SignUp />}></Route>
         <Route path="/About" element={<About />}></Route>
-
-    
+        <Route path="/Cart" element={<Cart />}></Route>    
       </Routes>
     </BrowserRouter>
   );
