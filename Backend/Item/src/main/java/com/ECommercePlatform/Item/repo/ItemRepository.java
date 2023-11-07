b27f3e7cfede54723ae94e8d0d2b965c43d108b9
Janath Gunarathna <95167638+JanathGunarathna@users.noreply.github.com>
2023-11-07 14:19:16 +0530

    add getAllItem

--- Backend/Item/src/main/java/com/ECommercePlatform/Item/repo/ItemRepository.java
+++ Backend/Item/src/main/java/com/ECommercePlatform/Item/repo/ItemRepository.java
@@ -4,10 +4,12 @@ import com.ECommercePlatform.Item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findById(long id);
+    List<Item> findByName(String name);
     //Optional<Item> findById(long id);
 
     //void deleteById(long id);
