a7a6131f64446be65fae0ea66670cf8d9e42ecdc
UserSampath <nalakasampathsmp@gmail.com>
2023-11-07 16:46:28 +0530

    Revert "add getAllItem"
    This reverts commit b27f3e7cfede54723ae94e8d0d2b965c43d108b9.

--- Backend/Item/src/main/java/com/ECommercePlatform/Item/controller/ItemController.java
+++ Backend/Item/src/main/java/com/ECommercePlatform/Item/controller/ItemController.java
@@ -1,6 +1,5 @@
 package com.ECommercePlatform.Item.controller;
 
-<<<<<<< HEAD
 import com.ECommercePlatform.Item.entity.Item;
 import com.ECommercePlatform.Item.repo.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,24 +21,6 @@ public class ItemController {
     ItemRepository itemRepository;
 
 
-    @GetMapping("/item")
-    public ResponseEntity<List<Item>> getAllItem(@RequestParam(required = false) String name)
-    {
-        try{
-            List<Item> items = new ArrayList<Item>();
-            if(name ==null)
-                itemRepository.findAll().forEach(items::add);
-            else
-                itemRepository.findByName(name).forEach(items::add);
-            if(items.isEmpty()){
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(items,HttpStatus.OK);
-        }catch (Exception e)
-        {
-            return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
     @GetMapping("/item/{id}")
     public ResponseEntity<Item> getItemById(@PathVariable("id") long id){
         Optional<Item> itemData = itemRepository.findById(id);
@@ -91,50 +70,3 @@ public class ItemController {
         }
     }
 }
-=======
-import com.ECommercePlatform.Item.dto.ItemDTO;
-import com.ECommercePlatform.Item.service.ItemService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping(value = "api/Item")
-@CrossOrigin
-public class ItemController {
-
-    @Autowired
-    public ItemService itemService;
-
-    @GetMapping("/get")
-    public List<ItemDTO> get()
-    {
-        return itemService.getAllItems();
-    }
-
-    @PostMapping("/save")
-    public ItemDTO save(@RequestBody ItemDTO itemDTO)
-    {
-        return itemService.save(itemDTO);
-    }
-    @PutMapping("/update")
-    public ItemDTO update(@RequestBody ItemDTO itemDTO)
-    {
-        return itemService.update(itemDTO);
-    }
-
-    @DeleteMapping("/delete")
-    public boolean delete(@RequestBody ItemDTO itemDTO)
-    {
-        return itemService.delete(itemDTO);
-    }
-
-    @GetMapping("/get/{userID}")
-    public ItemDTO getItemByID(@PathVariable String ID)
-    {
-        return itemService.getItemByID(ID);
-    }
-//add
-}
->>>>>>> main
--- Backend/Item/src/main/java/com/ECommercePlatform/Item/repo/ItemRepository.java
+++ Backend/Item/src/main/java/com/ECommercePlatform/Item/repo/ItemRepository.java
@@ -2,24 +2,13 @@ package com.ECommercePlatform.Item.repo;
 
 import com.ECommercePlatform.Item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Optional<Item> findById(long id);
-    List<Item> findByName(String name);
     //Optional<Item> findById(long id);
 
     //void deleteById(long id);
-=======
-import org.springframework.data.jpa.repository.Query;
-
-public interface ItemRepository extends JpaRepository<Item,Integer> {
-    @Query(value = "SELECT * FROM ITEM WHERE ID = ?1",nativeQuery = true)
-    Item getItemByID(String Id);
-
->>>>>>> main
 }
